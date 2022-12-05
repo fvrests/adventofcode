cbf1adf8906af0c4d23a638fb57519f3841cdbeb
fvrests <fvrests@icloud.com>
2022-12-04 19:33:19 -0500

    day 4

---
+++ day4/part2.ts
@@ -0,0 +1,22 @@
+const fileContents = await Deno.readTextFile("assignmentPairs.txt");
+
+const getSections = (assignment: string) => {
+  const [start, end] = assignment.split("-").map(Number);
+  const sections = [];
+  for (let i = start; i <= end; i++) {
+    sections.push(Number(i));
+  }
+  return sections;
+};
+
+let overlapCounter = 0;
+fileContents.split("\n").map((pair) => {
+  if (!pair) return;
+  const [elf1Sections, elf2Sections] = pair.split(",").map(getSections);
+  if (
+    elf1Sections.some((section) => elf2Sections.includes(section)) ||
+    elf2Sections.some((section) => elf1Sections.includes(section))
+  )
+    overlapCounter++;
+});
+console.log("overlapping assignments", overlapCounter);
