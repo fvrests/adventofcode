71ad9a18b9582a71a6d701280ae74f22e308c997
fvrests <fvrests@icloud.com>
2022-12-06 21:33:33 -0500

    day 6

---
+++ day6/part1.ts
@@ -0,0 +1,18 @@
+const datastream = await Deno.readTextFile("datastream.txt");
+
+const allLetters: string[] = [];
+
+console.log(
+	datastream
+		.trim()
+		.split("")
+		.findIndex((currentLetter) => {
+			allLetters.push(currentLetter);
+			if (allLetters.length < 4) return;
+			const currentGroup = allLetters.slice(-4);
+			return currentGroup.every(
+				(letter) =>
+					currentGroup.indexOf(letter) == currentGroup.lastIndexOf(letter)
+			);
+		}) + 1
+);
