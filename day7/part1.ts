1bc8cca221c2eab282efc1a6b7bec4a567517025
fvrests <fvrests@icloud.com>
2022-12-08 17:18:53 -0500

    day 7

---
+++ day7/part1.ts
@@ -0,0 +1,38 @@
+const terminalOutput = await Deno.readTextFile("terminal-output.txt");
+
+const folders: string[] = [];
+// @example: ['/', 'a', 'b']
+type Sizes = Record<string, number>;
+const sizes: Sizes = {};
+// @example: {'/': 600, '//a': 400,'//a/b': 200}
+
+const readLine = (line: string) => {
+	// starts with cd
+	const [, destination] = line.match(/\$\scd\s(.*)/) ?? [];
+	// starts with number
+	const [, fileSize] = line.match(/(\d+).*/) ?? [];
+
+	if (destination) {
+		if (destination === "..") folders.pop();
+		else folders.push(destination);
+	}
+
+	if (fileSize) {
+		const currentSize = Number(fileSize);
+		for (let i = folders.length; i >= 1; i--) {
+			const joinedPath = folders.slice(0, i).join("/");
+			sizes[joinedPath]
+				? (sizes[joinedPath] = sizes[joinedPath] + currentSize)
+				: (sizes[joinedPath] = currentSize);
+		}
+	}
+};
+
+terminalOutput.trim().split("\n").map(readLine);
+
+const sumSizesUnderLimit = (sizes: Sizes, limit: number) =>
+	Object.entries(sizes)
+		.filter(([_, size]) => size <= limit)
+		.reduce((total, [_, size]) => total + size, 0);
+
+console.log(sumSizesUnderLimit(sizes, 100_000));
